852c656909d7256cbefd63fda8e87fad0bb03f6b
jcheng-tech <johnny.cheng919@gmail.com>
2025-06-06 11:06:23 -0600

    Create capi-service-account.tf
    add capa-sa logic

---
+++ capi-service-account.tf
@@ -0,0 +1,42 @@
+
+resource "aws_iam_role" "capa_cluster_service_account_role" {
+  name = "${var.cluster_name}-cluster-service-account"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      },
+    ]
+  })
+
+  tags = merge(
+    {
+      "Name" : "${var.cluster_name}-cluster-service-account"
+    },
+    var.tags,
+  )
+}
+
+resource "aws_iam_policy" "capa_cluster_service_account_policy" {
+  name        = "${var.cluster_name}-cluster-service-account-policy"
+  description = "Policy for ${var.cluster_name} CAPI Cluster SA policy"
+  policy      = file("${path.module}/templates/aws/capa-cluster-service-account.json")
+
+  tags = merge(
+    {
+      "Name" : "${var.cluster_name}-cluster-service-account"
+    },
+    var.tags,
+  )
+}
+
+
+resource "aws_iam_role_policy_attachment" "capa_cluster_service_account_policy_attachment" {
+  role       = aws_iam_role.capa_cluster_service_account_role.name
+  policy_arn = aws_iam_policy.capa_cluster_service_account_policy.arn
+}
